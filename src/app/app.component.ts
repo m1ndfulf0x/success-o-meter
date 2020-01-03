dff69dd93b2bad233190acbc303ca5fad3047700
m1ndfulf0x <59140415+m1ndfulf0x@users.noreply.github.com>
2020-01-03 19:28:47 +0100

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/firestore";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'success-o-meter';
+
+  firebase.initializeApp(firebaseConfig);
 }
